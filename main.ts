2cb65f2bc726b38e67d90a40374f65096c200d81
Sebimala <86785937+Sebimala@users.noreply.github.com>
2024-04-27 11:22:32 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    Drink += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("" + (Drink + Food))
+})
+input.onButtonPressed(Button.B, function () {
+    Food += 1
+})
+let Food = 0
+let Drink = 0
+Drink = 0
+Food = 0
 basic.forever(function () {
 	
 })
